9017b4f474c603e64fe7febf8dc1cda815f65d7d
srecko23 <jakovsreckovic1@gmail.com>
2026-04-27 21:19:16 +0200

    refactor functions

--- index.js
+++ index.js
@@ -13,15 +13,22 @@ let points = 0;
 const options = document.getElementById("options");
 const gameUi = document.getElementById("game-ui");
 const wordDisplay = document.getElementById("word");
-const pointsDisplay = document.getElementById("points_display");
+const pointsDisplay = document.getElementById("points-display");
 const skipPenaltyDropdown = document.getElementById("penalty-dropdown");
 const difficultyMinInput = document.getElementById("difficulty-min");
 const difficultyMaxInput = document.getElementById("difficulty-max");
 const timeInput = document.getElementById("time");
 const timer = document.getElementById("timer");
 const lastGame = document.getElementById("last-game");
+const startButton = document.getElementById("start-button");
+const correctButton = document.getElementById("correct-button");
+const wrongButton = document.getElementById("wrong-button");
 
-window.startGame = () => {
+startButton.addEventListener("click", startGame);
+correctButton.addEventListener("click", correct);
+wrongButton.addEventListener("click", wrong);
+
+function startGame() {
     options.hidden = true;
     gameUi.hidden = false;
 
@@ -36,7 +43,7 @@ window.startGame = () => {
     incrementTimer();
 }
 
-window.nextWord = () => {
+function nextWord() {
     while (true) {
         let word = Object.keys(nouns)[Math.floor(Math.random() * Object.keys(nouns).length)];
         if (parseInt(nouns[word]) >= difficultyMin && parseInt(nouns[word]) <= difficultyMax) {
@@ -46,23 +53,23 @@ window.nextWord = () => {
     }
 }
 
-window.displayPoints = () => {
+function displayPoints() {
     pointsDisplay.textContent = "bodovi:" + points.toString()
 }
 
-window.correct = () => {
+function correct() {
     points++;
     nextWord();
     displayPoints();
 }
 
-window.wrong = () => {
+ function wrong() {
     points -= skipPenalty;
     nextWord();
     displayPoints();
 }
 
-window.incrementTimer = () => {
+function incrementTimer() {
     time = startTime - Math.floor((new Date() - startDate)/1000);
     timer.textContent = time;
     if (time == 0) {
